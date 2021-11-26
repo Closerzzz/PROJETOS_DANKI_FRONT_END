88cad344a7c7f8636186b3ff8268c7712b825f6d
Closerzzz <88691408+Closerzzz@users.noreply.github.com>
2021-11-26 14:43:40 -0300

    Add files via upload

---
+++ Projeto_Pratico_6/index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras,chaves,separadas,por,virgula">
+    <meta name="description" content="descrição do meu site">
+    <meta name="author" content="Autor do site">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <title>Projeto 06</title>
+</head>
+<body>
+    <section class="main">
+        <div class="layer"></div>
+        <div class="center">
+        <header>
+        <div class="icons-social">
+        <a href=""><i class="fab fa-facebook-f"></i></a>
+        <a href=""><i class="fab fa-instagram"></i></a>
+        </div><!--icons-->
+        <div class="clear"></div><!--clear-->
+            <div class="logo">
+            FaberCodeX
+            </div><!--logo-->
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav><!--desktop-->
+            <nav class="menu-mobile">
+                <i class="fas fa-bars"></i>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav><!---menumobile-->
+            <div class="clear"></div>
+        </header>
+        <div class="conteudo-header">
+            <h2>Bem-vindo ao nosso <span>Website</span></h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquet dictum mollis. In dictum, nisi convallis ultricies malesuada, odio mauris interdum magna, et efficitur risus velit vel elit. Phasellus non vulputate turpis. Curabitur massa libero, ultricies at mauris nec, efficitur cursus est. Nulla ultrices mattis leo et eleifend. Maecenas ac imperdiet est, ac tincidunt ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi iaculis scelerisque mi sit amet porta. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent condimentum urna et elit scelerisque, eget ornare tortor suscipit. Vivamus vel dolor imperdiet, aliquam lacus sodales, dapibus arcu. Phasellus vitae metus sollicitudin, sollicitudin leo non, scelerisque nisi. Vivamus vitae sodales diam. Aliquam vel nisl semper, placerat risus eget, tincidunt tortor. Quisque commodo, dolor et vestibulum porttitor, orci ipsum convallis augue, et scelerisque odio odio dignissim ante.
+
+            Praesent sed libero blandit, vehicula sapien a, gravida est. Aenean sit amet mauris sit amet ipsum pulvinar pharetra sed ac augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In sodales nec ligula ut molestie. Morbi vel porttitor ipsum. Nullam at leo a ex posuere efficitur. Morbi ut erat sit amet dui maximus dapibus.
+            </p>
+            <a href="">Saiba Mais</a>
+        </div><!--conteudo-header-->
+        </div><!--center-->
+    </section><!--main-->
+    
+    <section class="planos">
+        <div class="chamada-plano">
+        <div class="center">
+            <h2>Confira nossos planos</h2>
+        </div><!--center-->
+        </div><!--chamada-planos-->
+        <section class="box-planos">
+            <div class="center">
+                <div class="box-planos-single">
+                    <div class="box-planos-wraper">
+                    <h2>Lorem Ipsum</h2>
+                    <div class="pontos-plano">
+                        <p><i class="fas fa-check-square"></i>Lorem Ipsum silor dommor</p>
+                        <p><i class="fas fa-check-square"></i>Lorem Ipsum silor dommor</p>
+                        <p><i class="fas fa-check-square"></i>Lorem Ipsum silor dommor</p>
+                        <p><i class="fas fa-check-square"></i>Lorem Ipsum silor dommor</p>
+                    </div><!--pontos-plano-->
+                    <div class="preco">
+                        <h2>R$240,00</h2>
+                    </div><!--preco-->
+                </div>  
+                </div><!--box-plano-single-->
+                <div class="box-planos-single">
+                <div class="box-planos-wraper">
+                    <h2>Lorem Ipsum</h2>
+                    <div class="pontos-plano">
+                        <p><i class="fas fa-check-square"></i>Lorem Ipsum silor dommor</p>
+                        <p><i class="fas fa-check-square"></i>Lorem Ipsum silor dommor</p>
+                        <p><i class="fas fa-check-square"></i>Lorem Ipsum silor dommor</p>
+                        <p><i class="fas fa-check-square"></i>Lorem Ipsum silor dommor</p>
+                    </div><!--pontos-plano-->
+                    <div class="preco">
+                        <h2>R$210,00</h2>
+                    </div><!--preco-->
+                    </div>
+                </div><!--box-plano-single-->
+                <div class="box-planos-single">
+                <div class="box-planos-wraper">
+                    <h2>Lorem Ipsum</h2>
+                    <div class="pontos-plano">
+                        <p><i class="fas fa-check-square"></i>Lorem Ipsum silor dommor</p>
+                        <p><i class="fas fa-check-square"></i>Lorem Ipsum silor dommor</p>
+                        <p><i class="fas fa-check-square"></i>Lorem Ipsum silor dommor</p>
+                        <p><i class="fas fa-check-square"></i>Lorem Ipsum silor dommor</p>
+                    </div><!--pontos-plano-->
+                    <div class="preco">
+                        <h2>R$250,00</h2>
+                    </div><!--preco-->
+                    </div>
+                </div><!--box-plano-single-->   
+            </div><!--center-->
+        </section><!--box-planos-->
+    </section><!--planos-->
+    
+    <section class="paralax">
+                    <div class="overlay-paralax"></div><!--voerlay-->
+                <div class="center">
+            <h2>Conheça nossa <span style="color: #2692FF;">empresa</span></h2>
+        <p>Donec nec augue aliquam, tincidunt nibh et, ullamcorper est. Donec fermentum urna et nibh maximus auctor. Praesent posuere nunc metus. Quisque ut molestie eros. Donec eu felis nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus euismod arcu justo, sed dignissim enim rutrum in. Sed iaculis nisl purus, vel euismod elit facilisis vitae. Vestibulum quis efficitur tortor. Nunc vitae semper ipsum, sit amet viverra erat. Pellentesque vehicula fringilla odio, a ornare sapien condimentum eu. Nunc volutpat euismod augue, id molestie felis pharetra sed. Proin nec felis sapien. Donec feugiat sagittis commodo.</p>
+        <p>Vestibulum id mi eget enim blandit luctus eu ut tortor. Ut aliquet a sapien eu convallis. Etiam porta congue ante non vestibulum. Mauris tincidunt in tellus sed maximus. Suspendisse potenti. In condimentum ullamcorper luctus. Cras vitae risus vitae nibh consequat rhoncus quis in lacus.</p>
+                </div><!--center-->
+    </section><!--paralax-->
+    <section class="final-section">
+        <div class="center">
+            <div class="w50 contato">
+                <h2><div class="border-efeito"></div>Entre em contato</h2>
+                <form>
+                    <input type="text" placeholder="Insira seu nome...">
+                    <input type="email" placeholder="Insira seu e-mail...">
+                    <textarea placeholder="Sua mensagem"></textarea>
+                    <input type="submit" value="Enviar">
+                </form>
+            </div>
+            <!--w50-->
+            <div class="w50 time">
+                <h2><div class="border-efeito"></div>Conheça nosso time</h2>
+                <div class="w50 equipe-single">
+                    <div class="topo-box-usuario">
+                        <div class="box-avatar"></div>
+                        <div class="info-usuario">
+                            <p style="font-weight:bold">Giovanne Batista</p>
+                            <p style="font-style: italic">CEO / FOUNDER</p>
+                        </div>
+                    </div>
+                    <!--topo-box-usuario-->
+                    <div class="descricao-usuario-box">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu neque et nunc tincidunt aliquet. Ut posuere nulla eget risus ornare finibus. Duis tempor eleifend libero, nec scelerisque turpis viverra sed. Phasellus sed imperdiet purus. Integer efficitur tortor arcu. Nunc pretium eros libero, et porta nisl suscipit sed. Maecenas vestibulum elementum facilisis. Nullam pretium vitae erat quis euismod.</p>
+                    </div>
+                    <!--usuario-box-->
+                </div>
+                <div class="w50 equipe-single">
+                    <div class="topo-box-usuario">
+                        <div class="box-avatar"></div>
+                        <div class="info-usuario">
+                            <p style="font-weight:bold">Giovanne Batista</p>
+                            <p style="font-style: italic">CEO / FOUNDER</p>
+                        </div>
+                    </div>
+                    <!--topo-box-usuario-->
+                    <div class="descricao-usuario-box">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu neque et nunc tincidunt aliquet. Ut posuere nulla eget risus ornare finibus. Duis tempor eleifend libero, nec scelerisque turpis viverra sed. Phasellus sed imperdiet purus. Integer efficitur tortor arcu. Nunc pretium eros libero, et porta nisl suscipit sed. Maecenas vestibulum elementum facilisis. Nullam pretium vitae erat quis euismod.</p>
+                    </div>
+                    <!--usuario-box-->
+                </div>
+                <!--w50-->
+                <div class="clear"></div>
+            </div>
+            <!--w50CONTATO-->
+            <div class="clear"></div>
+        </div>
+        <!--center-->
+    </section>
+    <!---finalsection-->
+    <footer>
+        <p>Todos os direitos reservados</p>
+    </footer>
+<script src="https://kit.fontawesome.com/5b97f01e1a.js" crossorigin="anonymous"></script>
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/functions.js"></script>    
+</body>
+</html>
